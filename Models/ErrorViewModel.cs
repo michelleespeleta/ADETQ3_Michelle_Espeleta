91ecebd990308aa77945d9f14d70863b5fc12225
Michelle Espeleta <michelle_espeleta12@yahoo.com>
2020-10-17 20:09:42 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_Michelle_Espeleta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
